2fc83985c87a8d9bd225ef6f0b77391675cb0cb2
erizkiatama <erizkiatama@gmail.com>
2021-01-23 11:50:48 +0800

    Created test for GetAnimeCharacterStaff method

--- anime_test.go
+++ anime_test.go
@@ -9,23 +9,49 @@ import (
 func TestAnimeEndpoints(t *testing.T) {
 	Convey("Testing Anime Endpoints Method", t, func() {
 		client := NewJikanClient()
-		animeID := 1
-		animeStruct := Anime{}
 
-		Convey("GetAnime should return an Anime given valid ID", func() {
-			anime, err := client.GetAnime(animeID)
+		Convey("Testing GetAnime Method", func() {
+			animeID := 1
+			animeStruct := Anime{}
 
-			So(anime, ShouldHaveSameTypeAs, animeStruct)
-			So(anime.MalID, ShouldEqual, animeID)
-			So(err, ShouldBeNil)
+			Convey("GetAnime should return an Anime given valid ID", func() {
+				anime, err := client.GetAnime(animeID)
+
+				So(anime, ShouldHaveSameTypeAs, animeStruct)
+				So(anime.MalID, ShouldEqual, animeID)
+				So(err, ShouldBeNil)
+			})
+
+			Convey("GetAnime should return an error given invalid ID", func() {
+				anime, err := client.GetAnime(0)
+
+				So(anime, ShouldBeZeroValue)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, ResourceNotFoundError)
+			})
 		})
 
-		Convey("GetAnime should return an error given invalid ID", func() {
-			anime, err := client.GetAnime(0)
+		Convey("Testing GetAnimeCharacterStaff Method", func() {
+			animeID := 1
+			animeCharStaffStruct := AnimeCharacterStaff{}
 
-			So(anime, ShouldBeZeroValue)
-			So(err, ShouldNotBeNil)
-			So(err.Error(), ShouldEqual, "Resource does not exist")
+			Convey("GetAnimeCharacterStaff should return an AnimeCharacterStaff given valid ID", func() {
+				animeCharStaff, err := client.GetAnimeCharacterStaff(animeID)
+
+				So(animeCharStaff, ShouldHaveSameTypeAs, animeCharStaffStruct)
+				So(len(animeCharStaff.Characters), ShouldBeGreaterThanOrEqualTo, 0)
+				So(len(animeCharStaff.Staff), ShouldBeGreaterThanOrEqualTo, 0)
+				So(err, ShouldBeNil)
+			})
+
+			Convey("GetAnimeCharacterStaff should return an error given invalid ID", func() {
+				animeCharStaff, err := client.GetAnimeCharacterStaff(0)
+
+				So(animeCharStaff, ShouldBeZeroValue)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, ResourceNotFoundError)
+			})
 		})
 	})
+
 }
